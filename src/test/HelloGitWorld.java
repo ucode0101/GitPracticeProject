183cd33f320e9498f3bb7c373057a58fc60ea597
ucode0101 <ucode0101@gmail.com>
2023-10-13 14:47:06 -0400

    some message

--- src/test/HelloGitWorld.java
+++ src/test/HelloGitWorld.java
@@ -8,24 +8,10 @@ public class HelloGitWorld {
         System.out.println("Hello git push");
         // added this comment in GitHub
 
-         System.out.println("Hello Git World");
+        System.out.println("Hello Git World");
         System.out.println("Hello git World 2");
         System.out.println("Hello git push");
         // added this comment in GitHub
 
-        System.out.println("Hello git and github");
-        System.out.println("Hello git and github");
-        System.out.println("Hello git and github");
-
-        System.out.println("Hello git and github");
-        System.out.println("Hello git and github");
-        System.out.println("Hello git and github");
-
-        System.out.println("Hello there");
-        System.out.println("Hello git and github");
-        System.out.println("Hello git and github");
-        System.out.println("Hello git and github");
-
-        System.out.println("Hello there");
     }
 }
