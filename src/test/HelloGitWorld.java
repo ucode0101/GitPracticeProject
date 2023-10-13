868151cb500a017002bde530a4cc850567d8559b
U-Code Academy <136019202+ucode0101@users.noreply.github.com>
2023-10-13 12:08:15 -0400

    git pull practice

--- src/test/HelloGitWorld.java
+++ src/test/HelloGitWorld.java
@@ -7,5 +7,10 @@ public class HelloGitWorld {
         System.out.println("Hello git World 2");
         System.out.println("Hello git push");
         // added this comment in GitHub
+
+         System.out.println("Hello Git World");
+        System.out.println("Hello git World 2");
+        System.out.println("Hello git push");
+        // added this comment in GitHub
     }
 }
