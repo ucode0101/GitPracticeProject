27676d66c7c111ad061fe7bdfc38bab4171e8f2e
ucode0101 <ucode0101@gmail.com>
2023-10-12 21:44:32 -0400

    added some new code

--- src/test/Login.java
+++ src/test/Login.java
@@ -3,5 +3,8 @@ package test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Login to application");
+
+        System.out.println("Hello world");
+        System.out.println("I'm practicing some git and github");
     }
 }
