f1a04246b1167fb3152be772d05e08be9efae621
ucode0101 <ucode0101@gmail.com>
2023-10-12 21:46:54 -0400

    delete print statements

--- src/test/Login.java
+++ src/test/Login.java
@@ -2,9 +2,6 @@ package test;
 
 public class Login {
     public static void main(String[] args) {
-        System.out.println("Login to application");
 
-        System.out.println("Hello world");
-        System.out.println("I'm practicing some git and github");
     }
 }
