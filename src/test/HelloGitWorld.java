3ad8ff229ace5160d1f3d5b758466b6b39a79cb6
U-Code Academy <136019202+ucode0101@users.noreply.github.com>
2023-10-13 20:56:15 -0400

    Update HelloGitWorld.java

--- src/test/HelloGitWorld.java
+++ src/test/HelloGitWorld.java
@@ -4,24 +4,7 @@ public class HelloGitWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello Git World");
-        System.out.println("Hello git World 2");
-        System.out.println("Hello git push");
-        // added this comment in GitHub
 
-        System.out.println("Hello Git World");
-        System.out.println("Hello git World 2");
-        System.out.println("Hello git push");
-        // added this comment in GitHub
-
-          System.out.println("Hello Git World");
-        System.out.println("Hello git World 2");
-        System.out.println("Hello git push");
-        // added this comment in GitHub
-
-        System.out.println("Hello Git World");
-        System.out.println("Hello git World 2");
-        System.out.println("Hello git push");
-        // added this comment in GitHub
 
     }
 }
