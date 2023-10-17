97080766c2c93d89fbd7d1f2ffc8a49f6db68d0a
AminHasanov2000 <mastchoh2000@gmail.com>
2023-10-16 21:04:01 -0400

    Merge remote-tracking branch 'origin/Amin07' into Amin07

--- src/test/TestGitPullRequest.java
+++ src/test/TestGitPullRequest.java
@@ -3,5 +3,6 @@ package test;
 public class TestGitPullRequest {
     public static void main(String[] args) {
         System.out.println("hello pull request");
+        System.out.println("Commit push");
     }
 }
