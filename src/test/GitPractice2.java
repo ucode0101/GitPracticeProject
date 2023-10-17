af0463b86d274f10170ee43e569bbfb23b99bb02
AminHasanov2000 <147754079+AminHasanov2000@users.noreply.github.com>
2023-10-16 21:33:22 -0400

    Update GitPractice2.java

--- src/test/GitPractice2.java
+++ src/test/GitPractice2.java
@@ -3,7 +3,11 @@ package test;
 public class GitPractice2 {
     public static void main(String[] args) {
         System.out.println("hello there");
+        int num = 5;
+        System.out.println();
         System.out.println("hello there");
+        System.out.println("hello there");
+         System.out.println("hello there");
         System.out.println("hello there");
         
     }
